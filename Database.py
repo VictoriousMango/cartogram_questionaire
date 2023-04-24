1eb8846027992eba82225f8ebf301ab1868ecfe4
VictoriousMango <additya.mishra20@st.niituniversity.in>
2023-04-24 20:32:07 +0530

    Trying JSON

--- Database.py
+++ Database.py
@@ -2,9 +2,11 @@ import pymongo
 # from google.cloud import firestore
 import pyrebase
 import streamlit as st
+import json
 
 
 def app(Key, Ans):
+    '''
     client = pymongo.MongoClient('mongodb+srv://yadashesh:yadashesh@cluster0.lqsjbx0.mongodb.net/CartogramAssessment')
     db = client['CartogramAssessment']
     # collection = db['Answers']
@@ -12,7 +14,12 @@ def app(Key, Ans):
     post_id = posts.insert_one({Key: str(Ans)}).inserted_id
     print(post_id)
     st.write('Answers Saved')
-
+    '''
+    with open("DataBase.json", 'r') as readfile:
+        db = json.load(readfile)
+        db[Key] = Ans
+    with open("DataBase.json", "w") as outfile:
+        json.dump(db, outfile, indent=4)
 
 if __name__ == '__main__':
     print('Welcome To Pymongo')
