bd8734d8f52fd483cc0542d5bdfb6470c1290380
VictoriousMango <additya.mishra20@st.niituniversity.in>
2023-04-26 13:24:13 +0530

    testing deta database

--- Database.py
+++ Database.py
@@ -2,10 +2,37 @@ import pymongo
 # from google.cloud import firestore
 import pyrebase
 import streamlit as st
-import json
+from deta import Deta
 
+
+# Key : d0md8swdtzk_E7a3CqxYWfgL1E426fiH3gaBBSR2HgL6
 
 def app(Key, Ans):
+
+    # Load the environment variables
+    DETA_KEY = 'd0md8swdtzk_E7a3CqxYWfgL1E426fiH3gaBBSR2HgL6'
+
+    # Initialize with a project key
+    deta = Deta(DETA_KEY)
+
+    # This is how to create/connect a database
+    db = deta.Base("CartogramANS")
+
+    def insert_ans(Key, Ans):
+        """Returns the report on a successful creation, otherwise raises an error"""
+        return db.put({"key": Key, "Answers": Ans})
+
+    def fetch_all_periods():
+        """Returns a dict of all periods"""
+        res = db.fetch()
+        return res.items
+
+    def get_period(period):
+        """If not found, the function will return None"""
+        return db.get(period)
+
+    insert_ans(Key, Ans)
+    '''
     client = pymongo.MongoClient('mongodb+srv://yadashesh:yadashesh@cluster0.lqsjbx0.mongodb.net/CartogramAssessment')
     db = client['CartogramAssessment']
     # collection = db['Answers']
@@ -14,6 +41,7 @@ def app(Key, Ans):
     print(post_id)
     st.write('Answers Saved')
     '''
+    '''
     with open("Answer.json", 'r') as readfile:
         db = json.load(readfile)
         db[Key] = Ans
@@ -21,6 +49,7 @@ def app(Key, Ans):
         json.dump(db, outfile, indent=4)
     st.write('Answers Saved Successfully')
     '''
+
 
 if __name__ == '__main__':
     print('Welcome To Pymongo')
