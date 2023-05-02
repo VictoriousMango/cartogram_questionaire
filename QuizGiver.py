fc777d65643ef243a48a585de9849dca8b21d610
VictoriousMango <additya.mishra20@st.niituniversity.in>
2023-05-02 22:12:16 +0530

    Understanding Pattern 15

--- QuizGiver.py
+++ QuizGiver.py
@@ -141,6 +141,7 @@ def app(QuizDoc, time_stamp):
     def Questions(Index):
         if (st.session_state['Index'] + 1) % 36 == 0:
             st.balloons()
+            st.empty()
             SB.app(st.session_state['Index'] + 1//36)
 
         else:
