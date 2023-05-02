b5dc8271e509e9fc9e05476679eb332abd291d27
VictoriousMango <additya.mishra20@st.niituniversity.in>
2023-05-02 16:27:58 +0530

    Page Transitions

--- UserDetails.py
+++ UserDetails.py
@@ -15,22 +15,24 @@ def app():
     # Start = 0
     (Key, Name, Email, Gender, Age, UPPeople, Address, NOS, NOB, EduQual, CartoIdea) = (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
     if st.session_state['Start'] == 0:
-        # with ph.contianer():
-        st.title('Welcome User')
-        Name = st.text_input('Enter Your Name')
-        Email = st.text_input('Enter Your Email Address')
-        Gender = st.selectbox('Gender :', ['Male', 'Female', 'Others'])
-        Age = st.text_input('Enter Your Age')
-        UPPeople = st.selectbox('Are you from Uttar Pradesh', ['Yes', 'No'])
-        Address = st.text_input('Enter Your Address (Mention only District and State name): ')
-        NOS = st.text_input('Name of your School/College/Institution/Organization:')
-        NOB = st.text_input('Name of Your Board/University')
-        EduQual = st.selectbox('Your Highest Qualification: ', ['School', 'Graduation', 'Post-Graduation', 'Doctorate'])
-        CartoIdea = st.selectbox('Do you have any idea about Cartogram mapping?', ['Yes', 'No'])
-        st.session_state['Key'] = Name + Email
-        Save = st.button('Save', on_click=click)
-        if Save:
-            st.empty()
+        ShowContainer = st.container()
+        with ShowContainer:
+            # with ph.contianer():
+            st.title('Welcome User')
+            Name = ph.text_input('Enter Your Name')
+            Email = ph.text_input('Enter Your Email Address')
+            Gender = ph.selectbox('Gender :', ['Male', 'Female', 'Others'])
+            Age = ph.text_input('Enter Your Age')
+            UPPeople = ph.selectbox('Are you from Uttar Pradesh', ['Yes', 'No'])
+            Address = ph.text_input('Enter Your Address (Mention only District and State name): ')
+            NOS = ph.text_input('Name of your School/College/Institution/Organization:')
+            NOB = ph.text_input('Name of Your Board/University')
+            EduQual = ph.selectbox('Your Highest Qualification: ', ['School', 'Graduation', 'Post-Graduation', 'Doctorate'])
+            CartoIdea = ph.selectbox('Do you have any idea about Cartogram mapping?', ['Yes', 'No'])
+            st.session_state['Key'] = Name + Email
+            Save = ph.button('Save', on_click=click)
+            if Save:
+                st.empty()
     Start = st.session_state['Start']
     Key = st.session_state['Key']
     PersonalInfo = [Key, Name, Email, Gender, Age, UPPeople, Address, NOS, NOB, EduQual, CartoIdea]
